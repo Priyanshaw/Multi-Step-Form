e9850b303c5f8dabc59da91e8a08d8d36e5df01d
Priyanshu Raj <priyanshurocky5@gmail.com>
2024-08-07 16:19:03 +0530

    Setting up Stepper

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 import Header from './Components/Header/Header';
+import Stepper from './Components/Stepper/HorizontalLinearStepper';
 function App() {
   return (
     <>
-      <Header/>
+    <Header/>
+      <Container>
+        <Box sx={{ bgcolor: '#bfe2fd', height: '80vh', padding:'30px'}}>
+        <Stepper/>
+        </Box>
+      </Container>
+      
+      
     </>
   );
 }
