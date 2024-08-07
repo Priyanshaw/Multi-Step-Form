93d2c6f63377799abfaf4374d052b8718d4a348f
Priyanshu Raj <priyanshurocky5@gmail.com>
2024-08-07 15:53:11 +0530

    Implemented header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Header from './Components/Header/Header';
 function App() {
   return (
     <>
-      <h1>Multi Step Form</h1>
+      <Header/>
     </>
   );
 }
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <h1>Multi-Step Form</h1>
+  )
+}
+
+export default Header
